e1e8307bd0748beae1ae383122b9d0ac6f1f21f9
Alan Russell <ajrussellaudio@gmail.com>
2025-07-13 11:14:16 +0100

    fix: terraform syntax error

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,12 @@ resource "aws_dynamodb_table" "colours_table" {
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
 
-  attribute {
-    name = "id"
-    type = "S"
-  }
-}
+  resource "aws_dynamodb_table" "palettes_table" {
+  name           = "palettes"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
 
-attribute {
+  attribute {
     name = "id"
     type = "S"
   }
