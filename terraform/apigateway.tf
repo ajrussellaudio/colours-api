8f116555cf6b5077078673e710f5759547d68172
Alan Russell <ajrussellaudio@gmail.com>
2025-07-13 12:49:13 +0100

    fix: correct terraform reference for usage plan

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -24,5 +24,5 @@ resource "aws_api_gateway_usage_plan" "usage_plan" {
 resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
   key_id        = aws_api_gateway_api_key.api_key.id
   key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.id
+  usage_plan_id = aws_api_gateway_usage_plan.usage_plan.id
 }
