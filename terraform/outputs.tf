c4ad3bb15fe7df2f6d83e48610e26cc17ef39e09
Alan Russell <ajrussellaudio@gmail.com>
2025-07-13 11:32:32 +0100

    feat: add api gateway url to terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "dynamodb_table_name" {
 output "palettes_table_name" {
   value = aws_dynamodb_table.palettes_table.name
 }
+
+output "api_gateway_url" {
+  description = "The invoke URL for the API Gateway stage"
+  value       = aws_apigatewayv2_stage.api_stage.invoke_url
+}
